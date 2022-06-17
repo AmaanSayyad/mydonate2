e447809ee0e883522956e5b0a9c20fa4cccc5f69
mendsalbert <mendsalbert@gmail.com>
2022-06-17 07:40:06 +0000

    my donate v2

--- components/partials/Home.jsx
+++ components/partials/Home.jsx
@@ -175,140 +175,234 @@ function Home() {
         </div>
       </div>
 
-      <div className="relative flex flex-col space-y-8 ">
-        <p className="text-2xl py-4 dark:text-gray-100">Donations</p>
-        <div className="flex md:flex-row flex-col items-center justify-between w-full">
-          <div className="md:flex md:flex-row flex-col space-x-0  md:space-x-8">
-            <div class="w-full md:w-4/12 h-full">
-              <img
-                src="/images/dimage.jpeg"
-                className="object-cover h-48 rounded-2xl w-full"
-              />
+      {/* <div className="relative flex flex-col space-y-8 "> */}
+      <p className="text-2xl py-4 dark:text-gray-100">Donations</p>
+      <div className="grid grid-cols-4 space-y-8 ">
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <a href="#">
+            <img class="rounded-t-lg" src="/images/dimage.jpeg" alt="" />
+          </a>
+          <div class="p-5">
+            <a href="#">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Educare
+              </h5>
+            </a>
+            <p class="mb-3 font-normal text-lg text-gray-700 dark:text-gray-400">
+              Supporting Education in Africa
+            </p>
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="time-outline"
+                class="text-2xl dark:text-gray-100"
+              ></ion-icon>
+              <p className="text-xl dark:text-gray-100">15 days more</p>
             </div>
-            <div className="py-2">
-              <h2 className="text-3xl font-semibold dark:text-gray-200">
-                EDUCARE
-              </h2>
-              <p className="text-2xl dark:text-gray-100">
-                Supporting Education in Africa
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="wallet-outline"
+                class="text-2xl dark:text-gray-100"
+              ></ion-icon>
+              <p className="text-xl w-max dark:text-gray-200">
+                $1,000.00 / $1,000,000
               </p>
-              <div className="flex flex-row space-x-2 items-center">
-                <ion-icon
-                  name="time-outline"
-                  class="text-2xl dark:text-gray-100"
-                ></ion-icon>
-                <p className="text-xl dark:text-gray-100">15 days more</p>
-              </div>
-              <div className="flex flex-row space-x-2 items-center">
-                <ion-icon
-                  name="wallet-outline"
-                  class="text-2xl dark:text-gray-100"
-                ></ion-icon>
-                <p className="text-xl dark:text-gray-200">
-                  $1,000.00 / $10,000,000.00
-                </p>
-              </div>
-              <div className="flex flex-row space-x-2 items-center">
-                <ion-icon
-                  name="earth-outline"
-                  class="text-2xl dark:text-gray-200"
-                ></ion-icon>
-                <p className="text-xl dark:text-gray-100">Ghana</p>
-              </div>
             </div>
-          </div>
-          <div className="bg-gradient-to-r w-max from-indigo-500 via-purple-500 to-pink-500 mt-2 text-center px-6 py-3 rounded-full cursor-pointer text-white">
-            view
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="earth-outline"
+                class="text-2xl dark:text-gray-200"
+              ></ion-icon>
+              <p className="text-xl dark:text-gray-100">Ghana</p>
+            </div>
+            <a
+              href="#"
+              class="bg-gradient-to-r mt-4 from-indigo-500 via-purple-500 to-pink-500 inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                class="ml-2 -mr-1 w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
           </div>
         </div>
 
-        <div className="flex md:flex-row flex-col items-center justify-between w-full">
-          <div className="md:flex md:flex-row flex-col space-x-0  md:space-x-8">
-            <div class="w-full md:w-4/12 h-full">
-              <img
-                src="/images/dimage.jpeg"
-                className="object-cover h-48 rounded-2xl w-full"
-              />
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <a href="#">
+            <img class="rounded-t-lg" src="/images/dimage.jpeg" alt="" />
+          </a>
+          <div class="p-5">
+            <a href="#">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Educare
+              </h5>
+            </a>
+            <p class="mb-3 font-normal text-lg text-gray-700 dark:text-gray-400">
+              Supporting Education in Africa
+            </p>
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="time-outline"
+                class="text-2xl dark:text-gray-100"
+              ></ion-icon>
+              <p className="text-xl dark:text-gray-100">15 days more</p>
             </div>
-            <div className="py-2">
-              <h2 className="text-3xl font-semibold dark:text-gray-200">
-                EDUCARE
-              </h2>
-              <p className="text-2xl dark:text-gray-100">
-                Supporting Education in Africa
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="wallet-outline"
+                class="text-2xl dark:text-gray-100"
+              ></ion-icon>
+              <p className="text-xl w-max dark:text-gray-200">
+                $1,000.00 / $1,000,000
               </p>
-              <div className="flex flex-row space-x-2 items-center">
-                <ion-icon
-                  name="time-outline"
-                  class="text-2xl dark:text-gray-100"
-                ></ion-icon>
-                <p className="text-xl dark:text-gray-100">15 days more</p>
-              </div>
-              <div className="flex flex-row space-x-2 items-center">
-                <ion-icon
-                  name="wallet-outline"
-                  class="text-2xl dark:text-gray-100"
-                ></ion-icon>
-                <p className="text-xl dark:text-gray-200">
-                  $1,000.00 / $10,000,000.00
-                </p>
-              </div>
-              <div className="flex flex-row space-x-2 items-center">
-                <ion-icon
-                  name="earth-outline"
-                  class="text-2xl dark:text-gray-200"
-                ></ion-icon>
-                <p className="text-xl dark:text-gray-100">Ghana</p>
-              </div>
             </div>
-          </div>
-          <div className="bg-gradient-to-r w-max from-indigo-500 via-purple-500 to-pink-500 mt-2 text-center px-6 py-3 rounded-full cursor-pointer text-white">
-            view
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="earth-outline"
+                class="text-2xl dark:text-gray-200"
+              ></ion-icon>
+              <p className="text-xl dark:text-gray-100">Ghana</p>
+            </div>
+            <a
+              href="#"
+              class="bg-gradient-to-r mt-4 from-indigo-500 via-purple-500 to-pink-500 inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                class="ml-2 -mr-1 w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
           </div>
         </div>
 
-        <div className="flex md:flex-row flex-col items-center justify-between w-full">
-          <div className="md:flex md:flex-row flex-col space-x-0  md:space-x-8">
-            <div class="w-full md:w-4/12 h-full">
-              <img
-                src="/images/dimage.jpeg"
-                className="object-cover h-48 rounded-2xl w-full"
-              />
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <a href="#">
+            <img class="rounded-t-lg" src="/images/dimage.jpeg" alt="" />
+          </a>
+          <div class="p-5">
+            <a href="#">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Educare
+              </h5>
+            </a>
+            <p class="mb-3 font-normal text-lg text-gray-700 dark:text-gray-400">
+              Supporting Education in Africa
+            </p>
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="time-outline"
+                class="text-2xl dark:text-gray-100"
+              ></ion-icon>
+              <p className="text-xl dark:text-gray-100">15 days more</p>
             </div>
-            <div className="py-2">
-              <h2 className="text-3xl font-semibold dark:text-gray-200">
-                EDUCARE
-              </h2>
-              <p className="text-2xl dark:text-gray-100">
-                Supporting Education in Africa
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="wallet-outline"
+                class="text-2xl dark:text-gray-100"
+              ></ion-icon>
+              <p className="text-xl w-max dark:text-gray-200">
+                $1,000.00 / $1,000,000
               </p>
-              <div className="flex flex-row space-x-2 items-center">
-                <ion-icon
-                  name="time-outline"
-                  class="text-2xl dark:text-gray-100"
-                ></ion-icon>
-                <p className="text-xl dark:text-gray-100">15 days more</p>
-              </div>
-              <div className="flex flex-row space-x-2 items-center">
-                <ion-icon
-                  name="wallet-outline"
-                  class="text-2xl dark:text-gray-100"
-                ></ion-icon>
-                <p className="text-xl dark:text-gray-200">
-                  $1,000.00 / $10,000,000.00
-                </p>
-              </div>
-              <div className="flex flex-row space-x-2 items-center">
-                <ion-icon
-                  name="earth-outline"
-                  class="text-2xl dark:text-gray-200"
-                ></ion-icon>
-                <p className="text-xl dark:text-gray-100">Ghana</p>
-              </div>
             </div>
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="earth-outline"
+                class="text-2xl dark:text-gray-200"
+              ></ion-icon>
+              <p className="text-xl dark:text-gray-100">Ghana</p>
+            </div>
+            <a
+              href="#"
+              class="bg-gradient-to-r mt-4 from-indigo-500 via-purple-500 to-pink-500 inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                class="ml-2 -mr-1 w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
           </div>
-          <div className="bg-gradient-to-r w-max from-indigo-500 via-purple-500 to-pink-500 mt-2 text-center px-6 py-3 rounded-full cursor-pointer text-white">
-            view
+        </div>
+
+        <div class="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+          <a href="#">
+            <img class="rounded-t-lg" src="/images/dimage.jpeg" alt="" />
+          </a>
+          <div class="p-5">
+            <a href="#">
+              <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+                Educare
+              </h5>
+            </a>
+            <p class="mb-3 font-normal text-lg text-gray-700 dark:text-gray-400">
+              Supporting Education in Africa
+            </p>
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="time-outline"
+                class="text-2xl dark:text-gray-100"
+              ></ion-icon>
+              <p className="text-xl dark:text-gray-100">15 days more</p>
+            </div>
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="wallet-outline"
+                class="text-2xl dark:text-gray-100"
+              ></ion-icon>
+              <p className="text-xl w-max dark:text-gray-200">
+                $1,000.00 / $1,000,000
+              </p>
+            </div>
+            <div className="flex flex-row space-x-2 items-center">
+              <ion-icon
+                name="earth-outline"
+                class="text-2xl dark:text-gray-200"
+              ></ion-icon>
+              <p className="text-xl dark:text-gray-100">Ghana</p>
+            </div>
+            <a
+              href="#"
+              class="bg-gradient-to-r mt-4 from-indigo-500 via-purple-500 to-pink-500 inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            >
+              Read more
+              <svg
+                class="ml-2 -mr-1 w-4 h-4"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </a>
           </div>
         </div>
       </div>
