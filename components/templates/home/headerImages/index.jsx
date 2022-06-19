5c984457348d7b9e8a23a40c7c0eb7b6156c0e6c
mendsalbert <mendsalbert@gmail.com>
2022-06-19 23:05:26 +0000

    my donate v2

--- components/templates/home/headerImages/index.jsx
+++ components/templates/home/headerImages/index.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import GradientButton from '../../../utility/bottons/gradientButton';
 import Modal from '../../../utility/modal';
-import { ExclamationIcon } from '@heroicons/react/outline';
-
+import FundForm from '../../forms/fund';
 const Index = () => {
   const handleClick = () => {
     // alert('hey i am a reusable button');
@@ -52,31 +51,7 @@ const Index = () => {
           setModal(false);
         }}
       >
-        <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
-          <div className="sm:flex sm:items-start">
-            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
-              <ExclamationIcon
-                className="h-6 w-6 text-red-600"
-                aria-hidden="true"
-              />
-            </div>
-            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-              <p
-                as="h3"
-                className="text-lg leading-6 font-medium text-gray-900"
-              >
-                Deactivate account
-              </p>
-              <div className="mt-2">
-                <p className="text-sm text-gray-500">
-                  Are you sure you want to deactivate your account? All of your
-                  data will be permanently removed. This action cannot be
-                  undone.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
+        <FundForm />
       </Modal>
     </div>
   );
