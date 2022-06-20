294ea228dc05b71a228ba38cc74abcb22172a507
mendsalbert <mendsalbert@gmail.com>
2022-06-20 23:25:00 +0000

    my donate v2

--- components/templates/forms/fund.jsx
+++ components/templates/forms/fund.jsx
@@ -1,18 +1,32 @@
 import { UserGroupIcon, UserIcon } from '@heroicons/react/outline';
-import React, { useState } from 'react';
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 export default function Fund() {
+  const [countries, setcountries] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((response) => {
+        // console.log(response.data);
+        setcountries(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
+
   const [tab, setTab] = useState(1);
   const organization = (
-    <div className="px-4 py-5 dark:text-gray-200 bg-white rounded-2xl border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 space-y-6 sm:p-6">
+    <>
+      {/* <div className="px-4 py-5 dark:text-gray-200 bg-white rounded-2xl border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 space-y-6 sm:p-6"> */}
       <p className="text-xl text-center pt-3">Organization</p>
-      <div className="">
+      <div className="dark:text-gray">
         <div className="">
           <label
             htmlFor="company-website"
-            className="block text-md font-medium text-gray-700"
+            className="block text-md font-medium dark:text-gray-200 text-gray-700"
           >
-            Name of Organization
+            Name
           </label>
 
           <input
@@ -27,128 +41,131 @@ export default function Fund() {
         <div className="">
           <label
             htmlFor="company-website"
-            className="block text-md font-medium text-gray-700"
+            className="block text-md font-medium dark:text-gray-200 text-gray-700"
           >
-            Country of Organization
+            Country
           </label>
           <select
             id="countries"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           >
             <option selected>Choose a country</option>
-            <option value="US">United States</option>
-            <option value="CA">Canada</option>
-            <option value="FR">France</option>
-            <option value="DE">Germany</option>
+            {countries.map((country) => (
+              <option value="US">
+                {country.flag}
+                {country.name.common}
+              </option>
+            ))}
           </select>
         </div>
       </div>
-      <div>
-        <label
-          htmlFor="about"
-          className="block text-sm font-medium text-gray-700"
-        >
-          About
-        </label>
-        <div className="mt-1">
-          <textarea
-            id="about"
-            name="about"
-            rows={3}
-            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-            placeholder="you@example.com"
-            defaultValue={''}
-          />
-        </div>
-        <p className="mt-2 text-sm text-gray-500">
-          Brief description for your profile. URLs are hyperlinked.
-        </p>
-      </div>
 
-      <div>
-        <label className="block text-sm font-medium text-gray-700">Photo</label>
-        <div className="mt-1 flex items-center">
-          <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-            <svg
-              className="h-full w-full text-gray-300"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-            </svg>
-          </span>
-          <button
-            type="button"
-            className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Change
-          </button>
-        </div>
-      </div>
-
-      <div>
-        <label className="block text-sm font-medium text-gray-700">
-          Cover photo
-        </label>
-        <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-          <div className="space-y-1 text-center">
-            <svg
-              className="mx-auto h-12 w-12 text-gray-400"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
-            <div className="flex text-sm text-gray-600">
-              <label
-                htmlFor="file-upload"
-                className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-              >
-                <span>Upload a file</span>
-                <input
-                  id="file-upload"
-                  name="file-upload"
-                  type="file"
-                  className="sr-only"
-                />
-              </label>
-              <p className="pl-1">or drag and drop</p>
-            </div>
-            <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-
-  const individual = (
-    <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
-      <p className="text-xl text-center pt-3">Individual</p>
-      <div className="grid grid-cols-3 gap-6">
-        <div className="col-span-3 sm:col-span-2">
+      <div className=" flex flex-row justify-between space-x-4">
+        <div className="w-full">
           <label
             htmlFor="company-website"
-            className="block text-sm font-medium text-gray-700"
+            className="block text-md font-medium dark:text-gray-200 text-gray-700"
           >
-            Name of Organization
+            City
           </label>
-          <div className="mt-1 flex rounded-md shadow-sm">
-            <span className="inline-flex items-center px-3 rounded-l-md border border-r-0 border-gray-300 bg-gray-50 text-gray-500 text-sm">
-              http://
-            </span>
+          <input
+            type="text"
+            id="base-input"
+            class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+        <div className="w-full">
+          <label
+            htmlFor="company-website"
+            className="block text-md font-medium dark:text-gray-200 text-gray-700"
+          >
+            Address
+          </label>
+          <input
+            type="text"
+            id="base-input"
+            class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+      </div>
+
+      <div className="dark:text-gray">
+        <div className="">
+          <label
+            htmlFor="company-website"
+            className="block text-md font-medium dark:text-gray-200 text-gray-700"
+          >
+            Website
+          </label>
+
+          <input
+            type="text"
+            id="base-input"
+            class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          />
+        </div>
+      </div>
+
+      <div>
+        <label
+          htmlFor="company-website"
+          className="block text-md mb-2 font-medium dark:text-gray-200 text-gray-700"
+        >
+          Social media Handles
+        </label>
+        <div className=" flex flex-row justify-between space-x-4">
+          <div className="w-full">
+            <label
+              htmlFor="company-website"
+              className="block text-md font-medium dark:text-gray-200 text-gray-700"
+            >
+              Facebook
+            </label>
             <input
               type="text"
-              name="company-website"
-              id="company-website"
-              className="focus:ring-indigo-500 focus:border-indigo-500 flex-1 block w-full rounded-none rounded-r-md sm:text-sm border-gray-300"
-              placeholder="www.example.com"
+              id="base-input"
+              class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <div className="w-full">
+            <label
+              htmlFor="company-website"
+              className="block text-md font-medium dark:text-gray-200 text-gray-700"
+            >
+              Twitter
+            </label>
+            <input
+              type="text"
+              id="base-input"
+              class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+        </div>
+        <div className=" flex pt-8 flex-row justify-between space-x-4">
+          <div className="w-full">
+            <label
+              htmlFor="company-website"
+              className="block text-md font-medium dark:text-gray-200 text-gray-700"
+            >
+              Instagram
+            </label>
+            <input
+              type="text"
+              id="base-input"
+              class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+          <div className="w-full">
+            <label
+              htmlFor="company-website"
+              className="block text-md font-medium dark:text-gray-200 text-gray-700"
+            >
+              Youtube
+            </label>
+            <input
+              type="text"
+              id="base-input"
+              class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
           </div>
         </div>
@@ -156,67 +173,49 @@ export default function Fund() {
 
       <div>
         <label
-          htmlFor="about"
-          className="block text-sm font-medium text-gray-700"
+          htmlFor="company-website"
+          className="block text-md mb-2 font-medium dark:text-gray-200 text-gray-700"
         >
-          About
+          Purpose of Fund
         </label>
-        <div className="mt-1">
-          <textarea
-            id="about"
-            name="about"
-            rows={3}
-            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-            placeholder="you@example.com"
-            defaultValue={''}
+        <textarea
+          id="comment"
+          rows="4"
+          class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          //   class="px-0 w-full text-sm focus:outline-none text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+          placeholder="Purpose of fund"
+          required
+        ></textarea>
+      </div>
+
+      <div className="dark:text-gray">
+        <div className="">
+          <label
+            htmlFor="company-website"
+            className="block text-md font-medium dark:text-gray-200 text-gray-700"
+          >
+            Account for fund (eth)
+          </label>
+
+          <input
+            type="text"
+            id="base-input"
+            placeholder="0x0000000"
+            class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           />
         </div>
-        <p className="mt-2 text-sm text-gray-500">
-          Brief description for your profile. URLs are hyperlinked.
-        </p>
       </div>
 
       <div>
-        <label className="block text-sm font-medium text-gray-700">Photo</label>
-        <div className="mt-1 flex items-center">
-          <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
-            <svg
-              className="h-full w-full text-gray-300"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-            >
-              <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-            </svg>
-          </span>
-          <button
-            type="button"
-            className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Change
-          </button>
-        </div>
-      </div>
-
-      <div>
-        <label className="block text-sm font-medium text-gray-700">
-          Cover photo
+        <label className="block text-md font-medium dark:text-gray-200 text-gray-700">
+          Business Document
         </label>
         <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
           <div className="space-y-1 text-center">
-            <svg
-              className="mx-auto h-12 w-12 text-gray-400"
-              stroke="currentColor"
-              fill="none"
-              viewBox="0 0 48 48"
-              aria-hidden="true"
-            >
-              <path
-                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                strokeWidth={2}
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              />
-            </svg>
+            <ion-icon
+              name="document-outline"
+              class="text-4xl text-gray-600"
+            ></ion-icon>
             <div className="flex text-sm text-gray-600">
               <label
                 htmlFor="file-upload"
@@ -230,14 +229,15 @@ export default function Fund() {
                   className="sr-only"
                 />
               </label>
-              <p className="pl-1">or drag and drop</p>
             </div>
-            <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+            <p className="text-xs text-gray-500">PDF, DOCX, up to 10MB</p>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
+
+  const individual = <p>fdf</p>;
 
   return (
     <>
@@ -278,14 +278,13 @@ export default function Fund() {
               </div>
               <div className="shadow sm:rounded-2xl  sm:overflow-hidden">
                 {/* organization */}
-                {tab === 0 ? organization : tab === 1 ? individual : ''}
-                <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
-                  <button
-                    type="submit"
-                    className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                  >
-                    Save
-                  </button>
+                <div className="px-4 py-5 dark:text-gray-200 bg-white rounded-2xl border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 space-y-6 sm:p-6">
+                  {tab === 0 ? organization : tab === 1 ? individual : ''}
+
+                  <p className="text-xl text-center pt-3">
+                    Donation Information
+                  </p>
+                  <div></div>
                 </div>
               </div>
             </form>
