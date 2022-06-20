6ed9f6b03989544a5f0929856ea0674f93f5144d
mendsalbert <mendsalbert@gmail.com>
2022-06-20 22:12:12 +0000

    my donate v2

--- components/templates/forms/fund.jsx
+++ components/templates/forms/fund.jsx
@@ -4,8 +4,114 @@ import React, { useState } from 'react';
 export default function Fund() {
   const [tab, setTab] = useState(1);
   const organization = (
-    <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+    <div className="px-4 py-5 dark:text-gray-200 bg-white rounded-2xl border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700 space-y-6 sm:p-6">
       <p className="text-xl text-center pt-3">Organization</p>
+      <div className="">
+        <div className="">
+          <label
+            htmlFor="company-website"
+            className="block text-md font-medium text-gray-700"
+          >
+            Name of Organization
+          </label>
+          <div className="mt-1 flex rounded-md shadow-sm">
+            <input
+              type="text"
+              id="base-input"
+              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            />
+          </div>
+        </div>
+      </div>
+
+      <div>
+        <label
+          htmlFor="about"
+          className="block text-sm font-medium text-gray-700"
+        >
+          About
+        </label>
+        <div className="mt-1">
+          <textarea
+            id="about"
+            name="about"
+            rows={3}
+            className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
+            placeholder="you@example.com"
+            defaultValue={''}
+          />
+        </div>
+        <p className="mt-2 text-sm text-gray-500">
+          Brief description for your profile. URLs are hyperlinked.
+        </p>
+      </div>
+
+      <div>
+        <label className="block text-sm font-medium text-gray-700">Photo</label>
+        <div className="mt-1 flex items-center">
+          <span className="inline-block h-12 w-12 rounded-full overflow-hidden bg-gray-100">
+            <svg
+              className="h-full w-full text-gray-300"
+              fill="currentColor"
+              viewBox="0 0 24 24"
+            >
+              <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+            </svg>
+          </span>
+          <button
+            type="button"
+            className="ml-5 bg-white py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm leading-4 font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          >
+            Change
+          </button>
+        </div>
+      </div>
+
+      <div>
+        <label className="block text-sm font-medium text-gray-700">
+          Cover photo
+        </label>
+        <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+          <div className="space-y-1 text-center">
+            <svg
+              className="mx-auto h-12 w-12 text-gray-400"
+              stroke="currentColor"
+              fill="none"
+              viewBox="0 0 48 48"
+              aria-hidden="true"
+            >
+              <path
+                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+            <div className="flex text-sm text-gray-600">
+              <label
+                htmlFor="file-upload"
+                className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+              >
+                <span>Upload a file</span>
+                <input
+                  id="file-upload"
+                  name="file-upload"
+                  type="file"
+                  className="sr-only"
+                />
+              </label>
+              <p className="pl-1">or drag and drop</p>
+            </div>
+            <p className="text-xs text-gray-500">PNG, JPG, GIF up to 10MB</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+
+  const individual = (
+    <div className="px-4 py-5 bg-white space-y-6 sm:p-6">
+      <p className="text-xl text-center pt-3">Individual</p>
       <div className="grid grid-cols-3 gap-6">
         <div className="col-span-3 sm:col-span-2">
           <label
@@ -114,20 +220,14 @@ export default function Fund() {
     </div>
   );
 
-  const individaul = (
-    <div>
-      <p>Individual</p>
-    </div>
-  );
-
   return (
     <>
       <div>
         <div className="flex flex-col justify-center items-center ">
-          <div className="pt-14 md:mt-0 md:col-span-2  w-9/12">
+          <div className="pt-14 md:mt-0 md:col-span-2  w-8/12">
             <form action="#" method="POST">
               <div className="flex flex-row z-50 justify-center items-center">
-                <div className="bg-white -mb-5 shadow-md px-5 space-x-4 p-2 py-3 flex flex-row items-center rounded-full">
+                <div className="bg-white dark:bg-gray-800 dark:border-gray-700 dark:border-2 -mb-5 shadow-md px-5 space-x-4 p-2 py-3 flex flex-row items-center rounded-full">
                   <span
                     onClick={() => {
                       setTab(0);
@@ -136,9 +236,9 @@ export default function Fund() {
                     <UserGroupIcon
                       class={`${
                         tab === 0
-                          ? 'bg-blue-400 rounded-full px-4 text-white'
+                          ? 'bg-blue-400  rounded-full px-4 text-white'
                           : ''
-                      } h-8 cursor-pointer  `}
+                      } h-8 cursor-pointer dark:text-gray-200  `}
                     />
                   </span>
                   <span
@@ -151,14 +251,14 @@ export default function Fund() {
                         tab === 1
                           ? 'bg-blue-400 px-4 rounded-full text-white'
                           : ''
-                      } h-8 cursor-pointer  r`}
+                      } h-7 cursor-pointer dark:text-gray-200`}
                     />
                   </span>
                 </div>
               </div>
               <div className="shadow sm:rounded-2xl  sm:overflow-hidden">
                 {/* organization */}
-                {tab === 0 ? organization : tab === 1 ? individaul : ''}
+                {tab === 0 ? organization : tab === 1 ? individual : ''}
                 <div className="px-4 py-3 bg-gray-50 text-right sm:px-6">
                   <button
                     type="submit"
