5c6a41d1efa1cb86dd10e6da7d29b9bc562b63c4
mendsalbert <mendsalbert@gmail.com>
2022-06-20 23:36:30 +0000

    my donate v2

--- components/templates/forms/fund.jsx
+++ components/templates/forms/fund.jsx
@@ -284,7 +284,140 @@ export default function Fund() {
                   <p className="text-xl text-center pt-3">
                     Donation Information
                   </p>
-                  <div></div>
+                  <div className="space-y-4">
+                    <div className="dark:text-gray">
+                      <div className="">
+                        <label
+                          htmlFor="company-website"
+                          className="block text-md font-medium dark:text-gray-200 text-gray-700"
+                        >
+                          Title
+                        </label>
+
+                        <input
+                          type="text"
+                          id="base-input"
+                          class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
+                      </div>
+                    </div>
+
+                    <div>
+                      <label
+                        htmlFor="company-website"
+                        className="block text-md mb-2 font-medium dark:text-gray-200 text-gray-700"
+                      >
+                        Description
+                      </label>
+                      <textarea
+                        id="comment"
+                        rows="4"
+                        class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        //   class="px-0 w-full text-sm focus:outline-none text-gray-900 bg-white border-0 dark:bg-gray-800 focus:ring-0 dark:text-white dark:placeholder-gray-400"
+                        placeholder="Purpose of fund"
+                        required
+                      ></textarea>
+                    </div>
+
+                    <div className="">
+                      <div className="">
+                        <label
+                          htmlFor="company-website"
+                          className="block text-md font-medium dark:text-gray-200 text-gray-700"
+                        >
+                          Category
+                        </label>
+                        <select
+                          id="countries"
+                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        >
+                          <option selected>Choose a category</option>
+                          <option selected>War</option>
+                          <option selected>Health</option>
+                        </select>
+                      </div>
+                    </div>
+
+                    <div className=" flex flex-row justify-between space-x-4">
+                      <div className="w-full">
+                        <label
+                          htmlFor="company-website"
+                          className="block text-md font-medium dark:text-gray-200 text-gray-700"
+                        >
+                          End Date
+                        </label>
+                        <input
+                          type="date"
+                          id="base-input"
+                          class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
+                      </div>
+                      <div className="w-full">
+                        <label
+                          htmlFor="company-website"
+                          className="block text-md font-medium dark:text-gray-200 text-gray-700"
+                        >
+                          Target
+                        </label>
+                        <input
+                          type="text"
+                          id="base-input"
+                          class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                        />
+                      </div>
+                    </div>
+
+                    <div>
+                      <label className="block text-sm font-medium text-gray-700">
+                        Cover photo
+                      </label>
+                      <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
+                        <div className="space-y-1 text-center">
+                          <svg
+                            className="mx-auto h-12 w-12 text-gray-400"
+                            stroke="currentColor"
+                            fill="none"
+                            viewBox="0 0 48 48"
+                            aria-hidden="true"
+                          >
+                            <path
+                              d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                              strokeWidth={2}
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                            />
+                          </svg>
+                          <div className="flex text-sm text-gray-600">
+                            <label
+                              htmlFor="file-upload"
+                              className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
+                            >
+                              <span>Upload a file</span>
+                              <input
+                                id="file-upload"
+                                name="file-upload"
+                                type="file"
+                                className="sr-only"
+                              />
+                            </label>
+                            <p className="pl-1">or drag and drop</p>
+                          </div>
+                          <p className="text-xs text-gray-500">
+                            PNG, JPG, GIF up to 10MB
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="px-4 py-3 text-right sm:px-6">
+                      <button
+                        type="submit"
+                        className="inline-flex justify-center py-2 px-8 border border-transparent shadow-sm text-md  font-medium rounded-full text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                      >
+                        Save
+                      </button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </form>
