2073d1b94e50e277b553aa1e3712856377364a9c
mendsalbert <mendsalbert@gmail.com>
2022-06-21 15:36:23 +0000

    my donate v2

--- components/navigation/header/index.jsx
+++ components/navigation/header/index.jsx
@@ -190,9 +190,9 @@ function Header() {
                           href={item.href}
                           className={classNames(
                             item.current
-                              ? 'bg-gray-900 text-white'
-                              : 'text-gray-700 hover:bg-gray-700 hover:text-white',
-                            'px-3 py-2 rounded-md text-sm font-medium'
+                              ? 'bg-gray-900  text-white'
+                              : 'text-gray-700 dark:text-gray-200 hover:bg-gray-700 hover:text-white',
+                            'px-3 py-2 rounded-md text-md font-medium'
                           )}
                           aria-current={item.current ? 'page' : undefined}
                         >
@@ -204,16 +204,15 @@ function Header() {
                 </div>
                 <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                   <button
+                    onClick={
+                      colorTheme === 'light'
+                        ? () => setTheme('light')
+                        : colorTheme === 'dark'
+                        ? () => setTheme('dark')
+                        : ''
+                    }
                     type="button"
-                    className="bg-gray-200 p-1 rounded-full text-gray-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                  >
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-
-                  <button
-                    type="button"
-                    className=" p-1 rounded-full text-gray-600 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                    className=" p-1 rounded-full text-gray-600 "
                   >
                     <MoonIcon className="h-6 w-6" aria-hidden="true" />
                   </button>
