212409bca960bab3a585978e4fc3829f42231be8
mendsalbert <mendsalbert@gmail.com>
2022-06-21 15:52:32 +0000

    my donate v2

--- components/navigation/header/index.jsx
+++ components/navigation/header/index.jsx
@@ -215,7 +215,7 @@ function Header() {
                     className=" p-1 rounded-full text-gray-600 "
                   >
                     <MoonIcon
-                      className="h-6 w-6 dark:text-gray-200"
+                      className="h-8 w-8 dark:text-gray-200"
                       aria-hidden="true"
                     />
                   </button>
@@ -227,7 +227,7 @@ function Header() {
                         <span className="sr-only">Open user menu</span>
                         <img
                           className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          src="/images/profile3.jpg"
                           alt=""
                         />
                       </Menu.Button>
