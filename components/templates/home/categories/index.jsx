252c95dbf4ce899f469dccaf14ab0158848ab3fb
mendsalbert <mendsalbert@gmail.com>
2022-06-22 22:28:48 +0000

    my donate v2

--- components/templates/home/categories/index.jsx
+++ components/templates/home/categories/index.jsx
@@ -1,33 +1,112 @@
-import React from "react";
-import Card from "./card";
+import React from 'react';
+import Card from './card';
 const index = ({ name, color }) => {
+  return (
+    <div className="relative flex flex-col space-y-1 ">
+      <p className="text-2xl py-4 dark:text-gray-100">Trending Donations</p>
+
+      <div class="flex overflow-x-scroll pb-10 hide-scroll-bar snap-x">
+        <div className="flex flex-nowrap mt-5">
+          <div class="inline-block px-3 snap-center">
+            <Card
+              name={'Education'}
+              color={'bg-red-400'}
+              icon={'book-outline'}
+            />
+          </div>
+          <div class="inline-block px-3 snap-center">
+            <Card
+              name={'Education'}
+              color={'bg-red-400'}
+              icon={'book-outline'}
+            />
+          </div>
+          <div class="inline-block px-3 snap-center">
+            <Card
+              name={'Education'}
+              color={'bg-red-400'}
+              icon={'book-outline'}
+            />
+          </div>
+          <div class="inline-block px-3 snap-center">
+            <Card
+              name={'Education'}
+              color={'bg-red-400'}
+              icon={'book-outline'}
+            />
+          </div>
+          <div class="inline-block px-3 snap-center">
+            <Card
+              name={'Education'}
+              color={'bg-red-400'}
+              icon={'book-outline'}
+            />
+          </div>
+          <div class="inline-block px-3 snap-center">
+            <Card
+              name={'Education'}
+              color={'bg-red-400'}
+              icon={'book-outline'}
+            />
+          </div>
+          <div class="inline-block px-3 snap-center">
+            <Card
+              name={'Education'}
+              color={'bg-red-400'}
+              icon={'book-outline'}
+            />
+          </div>
+          <div class="inline-block px-3 snap-center">
+            <Card
+              name={'Education'}
+              color={'bg-red-400'}
+              icon={'book-outline'}
+            />
+          </div>
+        </div>
+        {/* </div> */}
+      </div>
+      <style></style>
+    </div>
+  );
   return (
     <>
       <p className="text-2xl py-2 dark:text-gray-100">Categories</p>
       <div className="relative grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 gap-6 w-full  md:gap-10 dark:text-gray-200 rounded-2xl pb-7">
         {/* <div className=" flex overflow-x-scroll scrollbar-hide md:scrollbar-hide space-x-4  font-Montserrat text-gray-700 text-left "> */}
         {/* <div className=" flex space-x-10 overflow-x-scroll  "> */}
-        <Card name={"Education"} color={"bg-red-400"} icon={"book-outline"} />
-        <Card
-          name={"Disaster"}
-          color={"bg-yellow-400"}
-          icon={"flask-outline"}
-        />
-        <Card name={"Health"} color={"bg-green-400"} icon={"pulse-outline"} />
-        <Card
-          name={"Famine"}
-          color={"bg-blue-400"}
-          icon={"fast-food-outline"}
-        />
-        <Card
-          name={"Community"}
-          color={"bg-pink-400"}
-          icon={"people-outline"}
-        />
-        <Card name={"War"} color={"bg-amber-600"} icon={"sad-outline"} />
-        {/* <Card name={"War"} color={"bg-amber-600"} icon={"sad-outline"} /> */}
+        <div class="flex overflow-x-scroll pb-10 hide-scroll-bar snap-x">
+          <div className="flex flex-nowrap mt-5">
+            <Card
+              name={'Education'}
+              color={'bg-red-400'}
+              icon={'book-outline'}
+            />
+            <Card
+              name={'Disaster'}
+              color={'bg-yellow-400'}
+              icon={'flask-outline'}
+            />
+            <Card
+              name={'Health'}
+              color={'bg-green-400'}
+              icon={'pulse-outline'}
+            />
+            <Card
+              name={'Famine'}
+              color={'bg-blue-400'}
+              icon={'fast-food-outline'}
+            />
+            <Card
+              name={'Community'}
+              color={'bg-pink-400'}
+              icon={'people-outline'}
+            />
+            <Card name={'War'} color={'bg-amber-600'} icon={'sad-outline'} />
+            {/* <Card name={"War"} color={"bg-amber-600"} icon={"sad-outline"} /> */}
+          </div>
+        </div>
       </div>
-      {/* </div> */}
     </>
   );
 };
