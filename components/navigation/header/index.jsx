de1d5ae2f76bd6ec1181483a27579d552dcc1e75
mendsalbert <mendsalbert@gmail.com>
2022-06-22 10:13:04 +0000

    my donate v2

--- components/navigation/header/index.jsx
+++ components/navigation/header/index.jsx
@@ -242,14 +242,14 @@ function Header() {
                       leaveFrom="transform opacity-100 scale-100"
                       leaveTo="transform opacity-0 scale-95"
                     >
-                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 dark:bg-gray-700 dark:border-gray-600 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                      <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 dark:bg-gray-700 dark:border-gray-600 dark:text-gray-200 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <Menu.Item>
                           {({ active }) => (
                             <a
                               href="#"
                               className={classNames(
                                 active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700'
+                                'block px-4 py-2 text-sm dark:text-gray-200 text-gray-700'
                               )}
                             >
                               Your Profile
