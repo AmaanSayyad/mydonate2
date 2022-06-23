c3f1e7abed23af171d9baa107baaebb62e77684f
mendsalbert <mendsalbert@gmail.com>
2022-06-23 07:49:37 +0000

    my donate v2

--- components/templates/donations/index.jsx
+++ components/templates/donations/index.jsx
@@ -5,8 +5,19 @@ import Donations from '../../templates/home/donations';
 import TrendingDonation from '../../templates/home/donations/trendingDonation';
 const Index = () => {
   return (
-    <div className="space-y-4 col-span-full lg:col-span-2">
-      <HeaderImage />
+    <div className="space-y-4 mt-8 col-span-full lg:col-span-2">
+      {/* <HeaderImage /> */}
+      <div class="pt-2 relative mx-auto text-gray-600">
+        <input
+          class="border-2 w-full border-gray-300 bg-white h-10 px-5 pr-16 rounded-lg text-sm focus:outline-none"
+          type="search"
+          name="search"
+          placeholder="Search"
+        />
+        <button type="submit" class="absolute right-0 top-0 mt-5 mr-4">
+          <ion-icon name="search-outline"></ion-icon>
+        </button>
+      </div>
       <Categories />
       <TrendingDonation />
       <Donations />
