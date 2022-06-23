a54885839baa2473ce6bbc078ac13f918e6b1d54
mendsalbert <mendsalbert@gmail.com>
2022-06-23 19:21:02 +0000

    my donate v2

--- components/partials/Donations.jsx
+++ components/partials/Donations.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import HeaderImage from '../templates/home/headerImages';
 import Categories from '../templates/home/categories';
 import Donations from '../templates/home/donations';
 import TrendingDonation from '../templates/home/donations/trendingDonation';
 import Modal from '../utility/modal';
+import axios from 'axios';
 const Donation = () => {
   const [modal, setModal] = useState(false);
+  const [countries, setcountries] = useState([]);
+  useEffect(() => {
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then((response) => {
+        // console.log(response.data);
+        setcountries(response.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
 
   return (
     <div className="space-y-4 mt-8 col-span-full lg:col-span-2">
@@ -18,8 +31,8 @@ const Donation = () => {
         className={`bg-gradient-to-r from-cyan-500 space-x-3 to-blue-500 mt-5 text-center w-max md:w-max items-center flex flex-row  px-6 py-3 rounded-full cursor-pointer text-white `}
       >
         <span>Filter</span>
-
-        <ion-icon name="funnel-outline" class="text-2xl"></ion-icon>
+        <ion-icon name="options-outline" class="text-2xl"></ion-icon>
+        {/* <ion-icon name="funnel-outline" class="text-2xl"></ion-icon> */}
       </div>
 
       <Categories />
@@ -32,10 +45,88 @@ const Donation = () => {
           setModal(false);
         }}
       >
-        <div class="border rounded overflow-hidden flex">
-          <input type="text" class="px-4 py-2 w-full" placeholder="Search..." />
-          <button class="flex items-center justify-center px-4 border-l">
-            <ion-icon name="search-outline"></ion-icon>
+        <p className="text-lg dark:text-gray-200">Filter Donations</p>
+        <div className="space-y-4">
+          <div className="dark:text-gray">
+            <div className="">
+              <label
+                htmlFor="company-website"
+                className="block text-md font-medium dark:text-gray-200 text-gray-700"
+              >
+                Search
+              </label>
+
+              <input
+                type="search"
+                placeholder="search......"
+                id="base-input"
+                class=" mt-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              />
+            </div>
+          </div>
+
+          <div className="">
+            <div className="">
+              <label
+                htmlFor="company-website"
+                className="block text-md font-medium dark:text-gray-200 text-gray-700"
+              >
+                Filter by Country
+              </label>
+              <select
+                id="countries"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option selected>Choose a country</option>
+                {countries.map((country) => (
+                  <option value="US">
+                    {country.flag}
+                    {country.name.common}
+                  </option>
+                ))}
+              </select>
+            </div>
+          </div>
+
+          <div className="">
+            <div className="">
+              <label
+                htmlFor="company-website"
+                className="block text-md font-medium dark:text-gray-200 text-gray-700"
+              >
+                Filter by Date
+              </label>
+              <select
+                id="countries"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option selected>Choose a country</option>
+              </select>
+            </div>
+          </div>
+
+          <div className="">
+            <div className="">
+              <label
+                htmlFor="company-website"
+                className="block text-md font-medium dark:text-gray-200 text-gray-700"
+              >
+                Filter by Category
+              </label>
+              <select
+                id="countries"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-3 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              >
+                <option selected>Choose a country</option>
+              </select>
+            </div>
+          </div>
+
+          <button
+            type="button"
+            className="w-full inline-flex justify-center rounded-full border border-transparent shadow-sm px-4 py-2 bg-blue-600 text-base font-medium text-white hover:bg-blue-700   sm:w-auto sm:text-sm"
+          >
+            Cancel
           </button>
         </div>
       </Modal>
--- components/utility/modal/index.jsx
+++ components/utility/modal/index.jsx
@@ -38,11 +38,11 @@ const Modal = ({ state, onClick, children }) => {
               leaveFrom="opacity-100 translate-y-0 sm:scale-100"
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
-              <Dialog.Panel className="relative bg-white rounded-2xl  text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
-                <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              <Dialog.Panel className="relative bg-white dark:bg-black rounded-2xl  text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:max-w-lg sm:w-full">
+                <div className="bg-white dark:bg-gray-800 px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                   {children}
                 </div>
-                <div className="bg-gray-50 border-t-2 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                <div className="bg-gray-50 dark:bg-gray-800 border-t-2 dark:border-black px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
                   <button
                     type="button"
                     className="w-full inline-flex justify-center rounded-full border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700  sm:ml-3 sm:w-auto sm:text-sm"
