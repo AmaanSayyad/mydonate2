ad0f2d10278567393dc2a38502a5ce1666d2ae55
mendsalbert <mendsalbert@gmail.com>
2022-06-23 07:31:21 +0000

    my donate v2

--- components/templates/donations/index.jsx
+++ components/templates/donations/index.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import HeaderImage from '../../templates/home/headerImages';
-import Categories from '../../templates/home/categoriess';
-import Donations from '../../templates/home/donationss';
-import TrendingDonation from '../../templates/home/trendingDonations';
+import Categories from '../../templates/home/categories';
+import Donations from '../../templates/home/donations';
+import TrendingDonation from '../../templates/home/donations/trendingDonation';
 const Index = () => {
   return (
     <div className="space-y-4 col-span-full lg:col-span-2">
