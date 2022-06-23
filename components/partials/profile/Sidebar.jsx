19f208e00658f97c97b90b2ea45355d0a469d172
mendsalbert <mendsalbert@gmail.com>
2022-06-23 22:39:18 +0000

    my donate v2

--- components/partials/profile/Sidebar.jsx
+++ components/partials/profile/Sidebar.jsx
@@ -94,53 +94,10 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
             </svg>
           </button>
           {/* Logo */}
-          <div className="flex flex-row items-center space-x-4">
+          <div className="flex flex-col  space-y-4">
             <img src="/images/logo.svg" class="w-9" />
-            <p className="text-white text-xl">myDonate</p>
+            {/* <p className="text-white text-xl">myDonate</p> */}
           </div>
-          {/* <div
-            end
-            to="/"
-            className="block ring-2 ring-white rounded-full w-max h-max"
-          >
-            <svg width="32" height="32" viewBox="0 0 32 32">
-              <defs>
-                <linearGradient
-                  x1="28.538%"
-                  y1="20.229%"
-                  x2="100%"
-                  y2="108.156%"
-                  id="logo-a"
-                >
-                  <stop stopColor="#A5B4FC" stopOpacity="0" offset="0%" />
-                  <stop stopColor="#A5B4FC" offset="100%" />
-                </linearGradient>
-                <linearGradient
-                  x1="88.638%"
-                  y1="29.267%"
-                  x2="22.42%"
-                  y2="100%"
-                  id="logo-b"
-                >
-                  <stop stopColor="#38BDF8" stopOpacity="0" offset="0%" />
-                  <stop stopColor="#38BDF8" offset="100%" />
-                </linearGradient>
-              </defs>
-              <rect fill="#6366F1" width="32" height="32" rx="16" />
-              <path
-                d="M18.277.16C26.035 1.267 32 7.938 32 16c0 8.837-7.163 16-16 16a15.937 15.937 0 01-10.426-3.863L18.277.161z"
-                fill="#4F46E5"
-              />
-              <path
-                d="M7.404 2.503l18.339 26.19A15.93 15.93 0 0116 32C7.163 32 0 24.837 0 16 0 10.327 2.952 5.344 7.404 2.503z"
-                fill="url(#logo-a)"
-              />
-              <path
-                d="M2.223 24.14L29.777 7.86A15.926 15.926 0 0132 16c0 8.837-7.163 16-16 16-5.864 0-10.991-3.154-13.777-7.86z"
-                fill="url(#logo-b)"
-              />
-            </svg>
-          </div> */}
         </div>
 
         {/* Links */}
@@ -148,7 +105,6 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
           {/* Pages group */}
           <div>
             <ul className="mt-3">
-              {/* Dashboard */}
               <li
                 className={`px-2 py-2 cursor-pointer rounded-sm mb-0.5 last:mb-0 `}
               >
@@ -157,9 +113,50 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   // className={`block text-slate-200 hover:text-white truncate transition duration-150 ${"hover:text-slate-200"}`}
                 >
                   <div className={`flex items-center`}>
-                    <ViewGridIcon className=" shrink-0 h-9 text-white" />
-                    <span className="text-sm font-medium ml-3 text-white lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                      Dashboard
+                    <ion-icon
+                      name="bar-chart-outline"
+                      class="text-white text-2xl"
+                    ></ion-icon>
+                    <span className="text-lg font-medium ml-3 text-white lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                      Summary
+                    </span>
+                  </div>
+                </Link>
+              </li>
+
+              <li
+                className={`px-2 py-2 cursor-pointer rounded-sm mb-0.5 last:mb-0 `}
+              >
+                <Link
+                  href={'/dashboard/customer/'}
+                  // className={`block text-slate-200 hover:text-white truncate transition duration-150 ${"hover:text-slate-200"}`}
+                >
+                  <div className={`flex items-center`}>
+                    <ion-icon
+                      name="wallet-outline"
+                      class="text-white text-2xl"
+                    ></ion-icon>
+                    <span className="text-lg font-medium ml-3 text-white lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                      My funds
+                    </span>
+                  </div>
+                </Link>
+              </li>
+
+              <li
+                className={`px-2 py-2 cursor-pointer rounded-sm mb-0.5 last:mb-0 `}
+              >
+                <Link
+                  href={'/dashboard/customer/'}
+                  // className={`block text-slate-200 hover:text-white truncate transition duration-150 ${"hover:text-slate-200"}`}
+                >
+                  <div className={`flex items-center`}>
+                    <ion-icon
+                      name="heart-outline"
+                      class="text-white text-2xl"
+                    ></ion-icon>
+                    <span className="text-lg font-medium ml-3 text-white lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                      My Donations
                     </span>
                   </div>
                 </Link>
