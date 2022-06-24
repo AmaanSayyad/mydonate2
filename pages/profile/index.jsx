7d69c5b461b8a56f2777bb4209c164de0b50d55f
mendsalbert <mendsalbert@gmail.com>
2022-06-24 06:23:59 +0000

    my donate v2

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -27,15 +27,7 @@ function Profile() {
               {/* Cards */}
               <div className="sm:flex sm:justify-end sm:items-center mb-8">
                 <div className="grid grid-flow-col sm:auto-cols-max justify-end sm:justify-end gap-2">
-                  <button className="px-4 py-2 flex flex-row items-center justify-center bg-indigo-500 rounded-md hover:bg-indigo-600 text-white">
-                    <svg
-                      className="w-4 h-4 fill-current opacity-50 shrink-0"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M15 7H9V1c0-.6-.4-1-1-1S7 .4 7 1v6H1c-.6 0-1 .4-1 1s.4 1 1 1h6v6c0 .6.4 1 1 1s1-.4 1-1V9h6c.6 0 1-.4 1-1s-.4-1-1-1z" />
-                    </svg>
-                    <span className=" xs:block ml-2">Add Product</span>
-                  </button>
+                  {/* button */}
                 </div>
               </div>
               <div className="grid grid-cols-12 gap-6"></div>
