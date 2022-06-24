5b39186f5197c580ba010500cf2eb8849a98cd08
mendsalbert <mendsalbert@gmail.com>
2022-06-24 06:39:18 +0000

    my donate v2

--- components/partials/profile/dashboard/WelcomeBanner.jsx
+++ components/partials/profile/dashboard/WelcomeBanner.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 function WelcomeBanner({ type, message, address }) {
   return (
-    <div className="relative bg-gradient-to-r from-green-300 via-blue-500 to-purple-600 py-10 px-10 rounded-md overflow-hidden mb-8 ">
+    <div className="relative bg-gradient-to-r from-purple-600 via-blue-500 to-green-300 py-10 px-10 rounded-md overflow-hidden mb-8 ">
       {/* Background illustration */}
       <div
         className="absolute right-0 top-0 -mt-4 mr-16 pointer-events-none hidden xl:block"
@@ -66,10 +66,12 @@ function WelcomeBanner({ type, message, address }) {
       </div>
       {/* Content */}
       <div className="relative">
-        <h1 className="text-2xl md:text-3xl text-slate-800 font-bold mb-1">
+        <h1 className="text-2xl md:text-3xl text-slate-200 font-bold mb-1">
           Welcome, Mends Albert👋
         </h1>
-        <p>{message || 'Here is what’s happening with your orders today:'}</p>
+        <p className="text-white">
+          {message || 'Here is what’s happening with your orders today:'}
+        </p>
       </div>
     </div>
   );
