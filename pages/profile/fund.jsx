795d94cee028b45da2a28d316c4970b0dff5f246
mendsalbert <mendsalbert@gmail.com>
2022-06-24 23:07:37 +0000

    my donate v2

---
+++ pages/profile/fund.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+
+import Sidebar from '../../components/partials/profile/Sidebar';
+import Header from '../../components/partials/profile/Header';
+import WelcomeBanner from '../../components/partials/profile/dashboard/WelcomeBanner';
+import StatisticCard from '../../components/partials/profile/dashboard/StatisticCard';
+
+function Fund() {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      <div className="flex h-screen overflow-hidden dark:bg-gray-900 font-Montserrat">
+        {/* Sidebar */}
+
+        <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+        {/* Content area */}
+        <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+          {/*  Site header */}
+          <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+          <main>
+            <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+              {/* Welcome banner */}
+              <WelcomeBanner type="Admin" />
+              {/* Cards */}
+              <div className="sm:flex sm:justify-end sm:items-center mb-8">
+                <div className="grid grid-flow-col sm:auto-cols-max justify-end sm:justify-end gap-2">
+                  {/* button */}
+                </div>
+              </div>
+              <div className="grid grid-cols-12 gap-6">
+                <p>fund 'add a new table component' </p>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Fund;
