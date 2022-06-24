66ac294ed7db211e51b0aa635c2630785532b7a2
mendsalbert <mendsalbert@gmail.com>
2022-06-24 23:17:01 +0000

    my donate v2

--- pages/profile/donation.jsx
+++ pages/profile/donation.jsx
@@ -40,89 +40,19 @@ function Donation() {
                     <thead>
                       <tr>
                         <th class="p-4 font-medium text-left text-gray-900 whitespace-nowrap">
-                          <div class="flex items-center">
-                            Name
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="w-4 h-4 ml-1.5 text-gray-700"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd"
-                              />
-                            </svg>
-                          </div>
+                          <div class="flex items-center">Title</div>
                         </th>
                         <th class="p-4 font-medium text-left text-gray-900 whitespace-nowrap">
-                          <div class="flex items-center">
-                            Email Address
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="w-4 h-4 ml-1.5 text-gray-700"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd"
-                              />
-                            </svg>
-                          </div>
+                          <div class="flex items-center">Description</div>
                         </th>
                         <th class="p-4 font-medium text-left text-gray-900 whitespace-nowrap">
-                          <div class="flex items-center">
-                            Status
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="w-4 h-4 ml-1.5 text-gray-700"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd"
-                              />
-                            </svg>
-                          </div>
+                          <div class="flex items-center">Status</div>
                         </th>
                         <th class="p-4 font-medium text-left text-gray-900 whitespace-nowrap">
-                          <div class="flex items-center">
-                            Phone Number
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="w-4 h-4 ml-1.5 text-gray-700"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd"
-                              />
-                            </svg>
-                          </div>
+                          <div class="flex items-center">Target</div>
                         </th>
                         <th class="p-4 font-medium text-left text-gray-900 whitespace-nowrap">
-                          <div class="flex items-center">
-                            Order Number
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              class="w-4 h-4 ml-1.5 text-gray-700"
-                              viewBox="0 0 20 20"
-                              fill="currentColor"
-                            >
-                              <path
-                                fill-rule="evenodd"
-                                d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                                clip-rule="evenodd"
-                              />
-                            </svg>
-                          </div>
+                          <div class="flex items-center">Scope</div>
                         </th>
                       </tr>
                     </thead>
@@ -130,7 +60,7 @@ function Donation() {
                     <tbody class="divide-y divide-gray-100">
                       <tr>
                         <td class="p-4 font-medium text-gray-900 whitespace-nowrap">
-                          John Doe
+                          Title{' '}
                         </td>
                         <td class="p-4 text-gray-700 whitespace-nowrap">
                           john.doe@email.com
@@ -169,16 +99,6 @@ function Donation() {
                       </tr>
 
                       <tr>
-                        <td class="sticky left-0 p-4 bg-white">
-                          <label class="sr-only" for="row_3">
-                            Row 3
-                          </label>
-                          <input
-                            class="w-5 h-5 border-gray-200 rounded"
-                            type="checkbox"
-                            id="row_3"
-                          />
-                        </td>
                         <td class="p-4 font-medium whitespace-nowrap">
                           Gary Barlow
                         </td>
