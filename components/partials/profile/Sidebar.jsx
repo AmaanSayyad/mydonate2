12f54dc614aeb65c8acf46e00666a1f2578c9b14
mendsalbert <mendsalbert@gmail.com>
2022-06-24 07:14:20 +0000

    my donate v2

--- components/partials/profile/Sidebar.jsx
+++ components/partials/profile/Sidebar.jsx
@@ -112,9 +112,9 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                   <div className={`flex items-center`}>
                     <ion-icon
                       name="bar-chart-outline"
-                      class="text-white text-2xl"
+                      class="text-gray-600 text-2xl"
                     ></ion-icon>
-                    <span className="text-lg font-medium ml-3 text-white lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
+                    <span className="text-lg font-medium ml-3 text-gray-600 lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
                       Summary
                     </span>
                   </div>
