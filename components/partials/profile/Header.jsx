7ebbfc189aa58cc1d6dc147f9b4fd0ecbcd64a6e
mendsalbert <mendsalbert@gmail.com>
2022-06-24 07:31:54 +0000

    my donate v2

--- components/partials/profile/Header.jsx
+++ components/partials/profile/Header.jsx
@@ -6,7 +6,7 @@ function Header({ sidebarOpen, setSidebarOpen }) {
   const [searchModalOpen, setSearchModalOpen] = useState(false);
 
   return (
-    <header className="sticky top-0 bg-white dark:bg-gray-700 border-b border-slate-200 dark:border-gray-800 z-30">
+    <header className="sticky top-0 bg-white dark:bg-gray-800 border-b border-slate-200 dark:border-gray-800 z-30">
       <div className="px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16 -mb-px">
           {/* Header: Left side */}
--- components/partials/profile/Sidebar.jsx
+++ components/partials/profile/Sidebar.jsx
@@ -70,7 +70,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
       <div
         id="sidebar"
         ref={sidebar}
-        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-24 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0  bg-white border-r-2 border-gray-300 p-4 transition-all duration-200 ease-in-out ${
+        className={`flex flex-col absolute z-40 left-0 top-0 lg:static lg:left-auto lg:top-auto lg:translate-x-0 transform h-screen overflow-y-scroll lg:overflow-y-auto no-scrollbar w-64 lg:w-24 lg:sidebar-expanded:!w-64 2xl:!w-64 shrink-0  bg-white dark:bg-gray-800 dark:border-gray-700 border-r-2 border-gray-300 p-4 transition-all duration-200 ease-in-out ${
           sidebarOpen ? 'translate-x-0' : '-translate-x-64'
         }`}
       >
@@ -79,7 +79,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
           {/* Close button */}
           <button
             ref={trigger}
-            className="lg:hidden text-gray-700 hover:text-slate-400"
+            className="lg:hidden text-gray-700 dark:text-gray-200 hover:text-slate-400"
             onClick={() => setSidebarOpen(!sidebarOpen)}
             aria-controls="sidebar"
             aria-expanded={sidebarOpen}
