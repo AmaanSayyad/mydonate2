999637048b25ac7a8430f86da2bdd469e0b0d7f7
mendsalbert <mendsalbert@gmail.com>
2022-06-27 22:05:53 +0000

    ‘temp’

--- components/partials/profile/dashboard/WelcomeBanner.jsx
+++ components/partials/profile/dashboard/WelcomeBanner.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 function WelcomeBanner({ type, message, address }) {
   return (
     <div className="relative bg-gradient-to-r from-purple-600 via-blue-500 to-green-300 py-10 px-6 rounded-xl overflow-hidden mb-8 ">
-      {/* Background illustration */}
       <div
         className="absolute right-0 top-0 -mt-4 mr-16 pointer-events-none hidden xl:block"
         aria-hidden="true"
