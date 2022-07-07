334d06a00a790748e02364b36d573adcf753dd41
mendsalbert <mendsalbert@gmail.com>
2022-07-07 16:45:02 +0000

    ‘mydonate’

--- pages/profile/fund.jsx
+++ pages/profile/fund.jsx
@@ -5,11 +5,12 @@ import Header from '../../components/partials/profile/Header';
 import WelcomeBanner from '../../components/partials/profile/dashboard/WelcomeBanner';
 import { AuthContext } from '../../utils/AuthProvider';
 import { truncateString } from '../../lib/utilities';
+import { ethers } from 'ethers';
 
 function Fund() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [open, setOpen] = useState(false);
-
+  let ethprice = 1270;
   const { signer, address } = useContext(AuthContext);
   const [donation, setdonation] = useState([]);
   useEffect(() => {
@@ -108,53 +109,16 @@ function Fund() {
                             )}
                           </td>
                           <td class="p-4 text-gray-700 whitespace-nowrap">
-                            USD - 10,000.00 (23 ETH)
+                            {ethers.utils.formatEther(
+                              donationItem.targetedAmount.toString()
+                            )}{' '}
+                            {'ETH'}
                           </td>
                           <td class="p-4 text-gray-700 whitespace-nowrap">
-                            Global
+                            {donationItem.user.country}
                           </td>
                         </tr>
                       ))}
-
-                      {/* <tr>
-                        <td class="p-4 font-medium text-gray-900 whitespace-nowrap">
-                          Donation to Africa...{' '}
-                        </td>
-                        <td class="p-4 text-gray-700 whitespace-nowrap">
-                          lets get rid of malaria.....
-                        </td>
-                        <td class="p-4 whitespace-nowrap">
-                          <strong class="bg-green-100 text-green-700 px-3 py-1.5 rounded text-xs font-medium">
-                            Accepted
-                          </strong>
-                        </td>
-                        <td class="p-4 text-gray-700 whitespace-nowrap">
-                          USD - 10,000.00 (23 ETH)
-                        </td>
-                        <td class="p-4 text-gray-700 whitespace-nowrap">
-                          Ghana
-                        </td>
-                      </tr>
-
-                      <tr>
-                        <td class="p-4 font-medium text-gray-900 whitespace-nowrap">
-                          Donation to Africa...{' '}
-                        </td>
-                        <td class="p-4 text-gray-700 whitespace-nowrap">
-                          lets get rid of malaria.....
-                        </td>
-                        <td class="p-4 text-gray-700 whitespace-nowrap">
-                          <strong class="bg-amber-100 text-amber-700 px-3 py-1.5 rounded text-xs font-medium">
-                            Pending
-                          </strong>
-                        </td>
-                        <td class="p-4 text-gray-700 whitespace-nowrap">
-                          USD - 10,000.00 (23 ETH)
-                        </td>
-                        <td class="p-4 text-gray-700 whitespace-nowrap">
-                          Nigeria
-                        </td>
-                      </tr> */}
                     </tbody>
                   </table>
                 </div>
