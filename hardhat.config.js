e80b143500d4f625246d997cad7b6c4a99ce1aa7
mendsalbert <mendsalbert@gmail.com>
2022-07-16 00:11:12 +0000

    ‘mydonatep’

--- hardhat.config.js
+++ hardhat.config.js
@@ -13,24 +13,18 @@ module.exports = {
       chainId: 1337,
       allowUnlimitedContractSize: true,
     },
-    // hardhat: {
-    //   throwOnTransactionFailures: true,
-    //   throwOnCallFailures: true,
-    //   allowUnlimitedContractSize: true,
-    //   blockGasLimit: 0x1fffffffffffff,
-    //   // accounts: accounts()
-    // },
-    //  unused configuration commented out for now
+
     mumbai: {
       url: 'https://rpc-mumbai.maticvigil.com',
       allowUnlimitedContractSize: true,
-
-      // url: "https://goerli.infura.io/v3/8b7ba5517c414450a93ec7334975a7fe",
+      accounts: [privateKey],
+    },
+    matic: {
+      url: 'https://polygon-mumbai.g.alchemy.com/v2/2bGIFu-iEnl9RvAOTe1ddZI2gBnuYQGS',
       accounts: [privateKey],
     },
     mainet: {
       url: `https://palm-mainnet.infura.io/v3/${projectId}`,
-      // accounts: [privateKey],
     },
     kovan: {
       url: 'https://kovan.infura.io/v3/745fcbe1f649402c9063fa946fdbb84c',
